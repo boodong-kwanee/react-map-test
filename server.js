0123a9f468eeaa8cac204573e375c186024eefed
boodong-rn <rhks122314@gmail.com>
2022-02-08 12:50:56 +0900

    Add: express and server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv/config");
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.use("/ping", (req, res) => {
+  res.json("pong");
+});
+
+app.use("/", (req, res) => {
+  console.log(__dirname);
+  // index.html 파일 응답
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`server is listening at PORT: ${PORT}`));
