390d9631a7a4bc4e5c56409f8eeb96b64f29342b
boodong-rn <rhks122314@gmail.com>
2022-02-08 13:16:09 +0900

    wip

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use("/ping", (req, res) => {
   res.json("pong");
 });
 
+app.use("/1", (req, res) => {
+  console.log(__dirname);
+  console.log("요청");
+  // index.html 파일 응답
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use("/", (req, res) => {
   console.log(__dirname);
   console.log("요청");
