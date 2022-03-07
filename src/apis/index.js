fc182316927c58d3d5502737928dfc2cf2b25c1e
boodong-rn <rhks122314@gmail.com>
2022-03-07 17:03:43 +0900

    Add: base url 변경

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "https://boodong.app/api/v2";
+export const baseURL = "https://boodong.app/api/v2/live";
 
 export const PublicInstance = axios.create({
   baseURL,
